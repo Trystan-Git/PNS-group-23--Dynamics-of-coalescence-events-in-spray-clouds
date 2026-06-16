b1da062792799163ea7de23dfbaa3d867881c8c6
Trystan-Git <trystan.van.wijk@gmail.com>
2026-06-16 17:00:35 +0200

    Created Outro.py
    This code makes the outro part of our manimation, it doesn't fully work yet.

---
+++ Manim/outro.py
@@ -0,0 +1,293 @@
+from manim import * 
+
+class outro(Scene):
+    def construct(self):
+
+        # text 
+        question = Text(r"Where do the droplets go?").shift(ORIGIN)
+        nose = Text(r"Nose").shift(DOWN*3 + LEFT*4)
+        nose_canal = Text(r"Nose canal").shift(DOWN*3)
+        airway = Text(r"Airway").shift(DOWN*3 + RIGHT*4)
+        results = Text(r"Our results").shift(UP*3.2)
+        diameter = Text(r"Diameter").scale(0.8)
+        velocity = Text(r"velocity").scale(0.8)
+        height = Text(r"height").scale(0.8)
+
+
+        #Waterdrops
+        def drop(**arguments):
+            return Circle(
+                color=BLUE,
+                fill_color=BLUE,
+                fill_opacity=0.8,
+                **arguments
+            )
+
+
+        #big droplets
+        bigdrop1 = drop(radius=0.32).shift(UP*2.5 + LEFT*5)
+        bigdrop2 = drop(radius=0.28).shift(UP*1.5 + RIGHT*3)
+        bigdrop3 = drop(radius=0.35).shift(DOWN*0.5 + LEFT*4)
+        bigdrop4 = drop(radius=0.26).shift(DOWN*2 + RIGHT*5)
+        bigdrop5 = drop(radius=0.31).shift(UP*0.5 + LEFT*1)
+        bigdrop6 = drop(radius=0.29).shift(DOWN*1.5 + RIGHT*1)
+        bigdrop7 = drop(radius=0.34).shift(UP*3 + RIGHT*5.5)
+        bigdrop8 = drop(radius=0.27).shift(DOWN*3 + LEFT*5.5)
+
+
+        # medium droplets
+
+        middrop1  = drop(radius=0.22).shift(UP*3.2 + LEFT*2.3)
+        middrop2  = drop(radius=0.18).shift(UP*2.3 + LEFT*0.8)
+        middrop3  = drop(radius=0.21).shift(UP*1.7 + RIGHT*0.7)
+        middrop4  = drop(radius=0.19).shift(UP*0.6 + LEFT*3.2)
+        middrop5  = drop(radius=0.23).shift(UP*0.4 + RIGHT*2.3)
+        middrop6  = drop(radius=0.20).shift(DOWN*0.4 + LEFT*1.2)
+        middrop7  = drop(radius=0.18).shift(DOWN*1.2 + RIGHT*0.3)
+        middrop8  = drop(radius=0.21).shift(DOWN*2.3 + LEFT*0.7)
+        middrop9  = drop(radius=0.22).shift(DOWN*2.1 + RIGHT*2.2)
+        middrop10 = drop(radius=0.19).shift(DOWN*3.2 + RIGHT*1.1)
+
+        # small droplets
+
+        smalldrop1  = drop(radius=0.11).shift(UP*3.1 + LEFT*3.4)
+        smalldrop2  = drop(radius=0.09).shift(UP*2.6 + LEFT*1.3)
+        smalldrop3  = drop(radius=0.10).shift(UP*2.1 + RIGHT*1.2)
+        smalldrop4  = drop(radius=0.10).shift(UP*1.6 + RIGHT*3.3)
+        smalldrop5  = drop(radius=0.08).shift(UP*1.2 + LEFT*3.8)
+        smalldrop6  = drop(radius=0.10).shift(UP*0.6 + LEFT*2.1)
+        smalldrop7  = drop(radius=0.09).shift(UP*0.4 + RIGHT*2.2)
+        smalldrop8  = drop(radius=0.11).shift(DOWN*0.6 + LEFT*3.2)
+        smalldrop9  = drop(radius=0.10).shift(DOWN*1.1 + RIGHT*1.6)
+        smalldrop10 = drop(radius=0.09).shift(DOWN*1.6 + RIGHT*3.1)
+        smalldrop11 = drop(radius=0.10).shift(DOWN*2.2 + LEFT*1.3)
+        smalldrop12 = drop(radius=0.08).shift(DOWN*2.6 + RIGHT*0.6)
+        smalldrop13 = drop(radius=0.11).shift(DOWN*3.1 + LEFT*2.7)
+        smalldrop14 = drop(radius=0.10).shift(DOWN*3.0 + RIGHT*2.1)
+        smalldrop15 = drop(radius=0.09).shift(ORIGIN + LEFT*1.2)
+
+
+        # sets of droplets
+
+        bigdrops = VGroup(bigdrop1,bigdrop2,bigdrop3,bigdrop4,bigdrop5,bigdrop6,bigdrop7,bigdrop8)
+
+        middrops = VGroup(middrop1,middrop2,middrop3,middrop4,middrop5,middrop6,middrop7,middrop8
+            ,middrop9,middrop10)
+
+        smalldrops = VGroup(smalldrop1,smalldrop2,smalldrop3,smalldrop4,smalldrop5,smalldrop6,smalldrop7
+            ,smalldrop8,smalldrop9,smalldrop10,smalldrop11,smalldrop12,smalldrop13,smalldrop14,smalldrop15)
+
+        drops = VGroup(bigdrops, middrops, smalldrops)
+
+
+        # Sorted droplet locations
+
+        big_targets = bigdrops.copy().arrange_in_grid(
+            rows=4,
+            cols=2,
+            buff=0.3
+        ).move_to(LEFT*4)
+
+        mid_targets = middrops.copy().arrange_in_grid(
+            rows=5,
+            cols=2,
+            buff=0.4
+        ).move_to(ORIGIN)
+
+        small_targets = smalldrops.copy().arrange_in_grid(
+            rows=5,
+            cols=3,
+            buff=0.6
+        ).move_to(RIGHT*4)
+
+
+        # Result boxes
+
+        def box():
+            return Square(
+                color=BLUE,
+                fill_color=BLUE,
+                fill_opacity=0,
+                side_length=3
+            )
+
+        box1 = box().shift(LEFT*4.5)
+        box2 = box().shift(ORIGIN)
+        box3 = box().shift(RIGHT*4.5)
+
+
+        # Greater than symbols and location
+
+        gt1 = VGroup(
+            Line(UP*0.3 + LEFT*0.2, RIGHT*0.2),
+            Line(DOWN*0.3 + LEFT*0.2, RIGHT*0.2),
+        ).scale(2)
+        gt2 = gt1.copy()
+        gt1.move_to((box1.get_center() + box2.get_center()) / 2)
+        gt2.move_to((box2.get_center() + box3.get_center()) / 2)
+
+        # Text position above boxes
+
+        diameter.next_to(box1, UP, buff=0.3)
+        velocity.next_to(box2, UP, buff=0.3)
+        height.next_to(box3, UP, buff=0.3)
+
+
+        # Playing animations
+
+        self.play(Write(question))
+        self.wait(0.6)
+        self.play(Unwrite(question))
+        self.play(
+            LaggedStart(
+                *[Create(d) for d in drops],
+            lag_ratio=0.1
+            )
+        )
+        self.wait(1)
+        self.play(
+            *[
+                d.animate.move_to(t.get_center())
+                for d, t in zip(bigdrops, big_targets)
+            ],
+            *[
+                d.animate.move_to(t.get_center())
+                for d, t in zip(middrops, mid_targets)
+            ],
+            *[
+                d.animate.move_to(t.get_center())
+                for d, t in zip(smalldrops, small_targets)
+            ],
+            run_time=3,
+        )
+
+        self.play(Create(nose))
+        self.wait(0.5)
+        self.play(Create(nose_canal))
+        self.wait(0.5)
+        self.play(Create(airway))
+        self.wait(0.5)
+        self.play(FadeOut(drops), 
+            FadeOut(nose), FadeOut(nose_canal), FadeOut(airway))
+        self.play(Create(results))
+        self.wait(0.5)
+        self.play(Create(box1), Create(box2), Create(box3))
+        self.play(Write(gt1))
+        self.wait(0.1)
+        self.play(Write(gt2))
+        self.play(
+            LaggedStart(
+                Write(diameter),
+                Write(velocity),
+                Write(height),
+                lag_ratio=0.4
+            )
+        )
+
+
+        # previous code from the examples resclaed
+
+        def make_nozzle():
+            return Rectangle(
+                color=GRAY,
+                height=2,
+                width=0.2,
+                fill_color=GRAY,
+                fill_opacity=0.8,
+            )
+ 
+        nozzle1 = make_nozzle()
+        nozzle2 = make_nozzle()
+        nozzle3 = make_nozzle()
+ 
+
+        # center reference point
+        center = box1.get_center() 
+
+        # main horizontal bar
+        meetbar = Line(
+            LEFT * 0.1+ DOWN*1.2, RIGHT * 0.1 + DOWN * 1.2, color=WHITE)
+
+        # vertical caps (perpendicular)
+        zijbarL = Line(
+            DOWN * 0.1,  DOWN * 0.2, color=WHITE)
+
+        zijbarR = Line(
+            DOWN *0.1, DOWN *0.2, color=WHITE)
+
+        # group them first
+        bar1 = VGroup(meetbar, zijbarL, zijbarR, nozzle1)
+
+        # position whole structure
+        bar1.move_to(center)
+
+        # align pieces relative to meetbar
+        zijbarL.next_to(meetbar, LEFT, buff=0)
+        zijbarR.next_to(meetbar, RIGHT, buff=0)
+ 
+        # Position nozzle3 
+        nozzle3.move_to(box3.get_center())
+ 
+        # Height bar: tracks the bottom of nozzle3
+        ground_y = -1.5  # fixed "ground" level
+        bar_x = nozzle3.get_center()[0] + 0.6 
+
+
+ 
+        def make_height_bar():
+            top_y = nozzle3.get_bottom()[1]
+            return VGroup(
+                Line(ground_y*UP+4.3*RIGHT, top_y, color=WHITE),
+                Line([bar_x - 0.1, ground_y+0.7, 0], [bar_x + 0.1, ground_y+0.7, 0], color=WHITE).scale(0.6),
+                Line([bar_x - 0.1, top_y, 0], [bar_x + 0.1, top_y, 0], color=WHITE).scale(0.6),
+            )
+ 
+        bar2 = always_redraw(make_height_bar)
+
+ 
+        # Velocity arrow & droplets 
+        snelpijl = Arrow(start=RIGHT + UP, end=RIGHT + DOWN * 0.4).scale(0.6)
+ 
+        minidrops = VGroup(*[
+            Circle(radius=0.05, color=BLUE, fill_opacity=0.8).scale(0.6)
+            for _ in range(50)
+        ]).shift(box2.get_top() + DOWN * 0.6)
+ 
+        # Parameter 1: Diameter 
+        self.play(FadeIn(bar1))
+        self.play(bar1.animate.stretch(2, dim=0), run_time=1.2)
+        self.play(bar1.animate.stretch(0.25, dim=0), run_time=1.2) 
+        self.play(bar1.animate.stretch(2, dim=0), run_time=1.2)
+        self.wait(0.5)
+ 
+        # Parameter 2: Velocity 
+        self.play(FadeIn(nozzle2))
+        self.play(FadeIn(snelpijl), FadeIn(minidrops))
+        self.play(nozzle2.animate.shift(box2.get_top() + DOWN * 0.35).stretch(0.3, dim=1))
+        sync_time = 5 
+        
+        self.play(        
+            # The arrow stretches out and shrinks back
+            snelpijl.animate(
+                run_time=sync_time,
+                rate_func=there_and_back
+            ).stretch(1.7, dim=1),
+
+            LaggedStart(
+                *[
+                    c.animate(rate_func=smooth).shift(DOWN * 6) 
+                    for c in minidrops
+                ],
+                lag_ratio=0.04,
+                run_time=sync_time,
+                rate_func=smooth 
+            ),
+        )
+
+        # Parameter 3: Height 
+        self.play(FadeIn(nozzle3), FadeIn(bar2))
+        self.play(nozzle3.animate.shift(UP*0.35), run_time=1.2)
+        self.play(nozzle3.animate.shift(DOWN * 0.35), run_time=1.2)  
+ 
+        self.wait()
+
